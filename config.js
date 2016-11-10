290a80e4d238783bea9b78dc4ba3e40cdd3cfb54
gbarseghyan <gbarseghyan@vtgsoftware.com>
2016-11-10 18:51:40 +0400

    - code clean

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         baseUrl : "http://176.32.196.113:8000",
         secret: 'c6ddbf5047efc9s4e0d8ff9a87f4b5acb92abb8sdd26662ff2ddc74e33d1e24e0af7ssaa904825ae632e967418s98b1effd06531s15637cdca372bff0004f035',
         mongo_url: 'mongodb://127.0.0.1:27017/maeuticaAgenda',
-        SENDGRID_API_KEY : "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",
+        SENDGRID_API_KEY : "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",
         EMAIL_FROM : "purchase@ticket",
         http_port: 8000,
         http_host: '0.0.0.0'
@@ -12,7 +12,7 @@ module.exports = {
         baseUrl : "http://37.186.125.214:8080",
         secret: 'c6ddbf5047efc9s4e0d8ff9a87f4b5acb92abb8sdd26662ff2ddc74e33d1e24e0af7ssaa904825ae632e967418s98b1effd06531s15637cdca372bff0004f035',
         mongo_url: 'mongodb://localhost/brandsAndBusinessConference',
-        SENDGRID_API_KEY : "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",
+        SENDGRID_API_KEY : "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",
         EMAIL_FROM : "purchase@ticket",
         http_port: 8080,
         http_host: '0.0.0.0'
